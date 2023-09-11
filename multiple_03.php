bc795faae88007ee1157f0a657129ae454365ba0
cdavidson8 <142449915+cdavidson8@users.noreply.github.com>
2023-09-10 22:30:13 -0500

    Update multiple_03.php

--- multiple_03.php
+++ multiple_03.php
@@ -1,39 +1,51 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SESSION['user_data']['address'] = $_POST['address'];
+    $_SESSION['user_data']['city'] = $_POST['city'];
+    $_SESSION['user_data']['state'] = $_POST['state'];
+    $_SESSION['user_data']['country'] = $_POST['country'];
+    header("Location: multiple_04.php");
+    exit();
+}
+?>
+
 <!DOCTYPE HTML>
 <html>
-<head>
-<title>Multiple form 3</title>
-</head>
-
-<body>
-<form method="head" action="">
-<p>
-<label for="address">Address:</label>
-<input type="text" name="address" id="address_id">
-</p>
-<p>
-<label for="city">City:</label>
-<input type="text" name="city" id="city_id" required>
-</p>
-<p>
-<label for="state">State:</label>
-<input type="text" name="state" id="state_id" required>
-</p>
-<p>
-<label for="country">Country:</label>
-<select name="country" id="country_id" required>
-<option value="" selected>Please Select</option>
-<option value="Canada">Canada</option>
-<option value="France">France</option>
-<option value="Germany">Germany</option>
-<option value="Japan">Japan</option>
-<option value="UK">United Kingdom</option>
-<option value="USA">United States</option>
-<option value="other">Other</option>
-</select>
-</p>
-<p>
-<input type="submit" name="next" value="Send details">
-</p>
-</form>
-</body>
+    <head>
+        <title>Form 3</title>
+    </head>
+    <body>
+        <form method="POST" action="">
+            <p>
+                <label for="address">Address:</label>
+                <input type="text" name="address" id="address_id">
+            </p>
+            <p>
+                <label for="city">City:</label>
+                <input type="text" name="city" id="city_id" required>
+            </p>
+            <p>
+                <label for="state">State:</label>
+                <input type="text" name="state" id="state_id" required>
+            </p>
+            <p>
+                <label for="country">Country:</label>
+                <select name="country" id="country_id" required>
+                    <option value="" selected>Please Select</option>
+                    <option value="Canada">Canada</option>
+                    <option value="France">France</option>
+                    <option value="Germany">Germany</option>
+                    <option value="Japan">Japan</option>
+                    <option value="UK">United Kingdom</option>
+                    <option value="USA">United States</option>
+                    <option value="other">Other</option>
+                </select>
+            </p>
+            <p>
+                <input type="submit" name="next" value="Send details">
+            </p>
+        </form>
+    </body>
 </html>
